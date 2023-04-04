cd086cc023cc73b6839308654265e49041ec8d71
Vlad Bolofinov <vlad.bolofinov@gmail.com>
2023-04-04 13:11:54 +0300

    upload new project

---
+++ src/components/app/App.js
@@ -0,0 +1,22 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import AppHeader from "../appHeader/AppHeader";
+import {MainPage, ComicsPage} from '../pages';
+
+const App = () => {
+
+        return (
+            <Router>
+                <div className="app">
+                    <AppHeader/>
+                    <main>
+                        <Routes>
+                            <Route path="/comics" element={<ComicsPage/>}/>
+                            <Route path="/" element={<MainPage/>}/>
+                        </Routes>
+                    </main>
+                </div>
+            </Router>
+        )
+}
+
+export default App;
